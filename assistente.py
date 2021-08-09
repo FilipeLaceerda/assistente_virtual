0bc9d22f93a07368c42f578de3b93402036b6095
FilipeLaceerda <filipeelacerda@gmail.com>
2021-08-09 01:31:11 -0300

    assistente_virtual

---
+++ assistente.py
@@ -0,0 +1,115 @@
+import speech_recognition as sr
+from nltk import word_tokenize, corpus
+import json
+
+IDIOMA = "portuguese"
+IDIOMA_FALA = "pt_BR"
+CONFIGURACAO = "config.json"
+
+
+def iniciar():
+    global reconhecedor
+    global palavras_de_parada
+    global nome_assistente
+    global acoes
+    
+    reconhecedor = sr.Recognizer()
+    palavras_de_parada = set(corpus.stopwords.words(IDIOMA))
+    
+    
+    with open(CONFIGURACAO, "r") as arquivo_configuracao:
+        configuracao = json.load(arquivo_configuracao)
+
+        nome_assistente = configuracao["nome"]
+        acoes = configuracao["acoes"]
+
+        arquivo_configuracao.close()
+
+
+def escutar_comando():
+    global reconhecedor
+    
+    comando = None
+    
+    with sr.Microphone() as fonte_audio:
+        reconhecedor.adjust_for_ambient_noise(fonte_audio)
+        
+        print("Olá, o que quer que eu faça?")        
+        fala = reconhecedor.listen(fonte_audio, timeout=5, phrase_time_limit=5)
+        try:
+            comando = reconhecedor.recognize_google(fala, language=IDIOMA_FALA)
+        except sr.UnknownValueError:
+            pass
+    
+    return comando
+    
+
+
+def eliminar_palavras_de_parada(tokens):
+    global palavras_de_parada
+    
+    tokens_filtrados = []
+    for token in tokens:
+        if token not in palavras_de_parada:
+            tokens_filtrados.append(token)
+    
+    return tokens_filtrados
+
+
+def tokenizar_comando(comando):
+    global nome_assistente
+    
+    acao = None
+    objeto = None
+    
+    tokens = word_tokenize(comando, IDIOMA)
+    if tokens:
+        tokens = eliminar_palavras_de_parada(tokens)
+        
+        if len(tokens) >= 3:
+            if nome_assistente == tokens[0].lower():
+                acao = tokens[1].lower()
+                objeto = tokens[2].lower()            
+    
+    return acao, objeto
+    
+
+def validar_comando(acao, objeto):
+    global acoes
+    
+    valido = False
+    
+    if acao and objeto:
+        for acaoCadastrada in acoes:
+            if acao == acaoCadastrada["nome"]:
+                if objeto in acaoCadastrada["objeto"]:
+                    valido = True
+                    
+                break
+    
+    return valido
+
+def executar_comando(acao, objeto):
+    print("vou executar o comando:", acao, objeto)
+       
+
+if __name__ == '__main__':
+    iniciar()
+
+    continuar = True
+    while continuar:
+        try:
+            comando = escutar_comando()
+            print(f"processando o comando: {comando}")
+
+            if comando:
+                acao, objeto = tokenizar_comando(comando)
+                valido = validar_comando(acao, objeto)
+                if valido:
+                    executar_comando(acao, objeto)
+                else:
+                    print("Não entendi o comando. Repita, por favor!")
+        except KeyboardInterrupt:
+            print("Até mais!")
+
+            continuar = False
---
+++ download.py
@@ -0,0 +1,3 @@
+import ntlk
+
+ntlk.download('punkt')
